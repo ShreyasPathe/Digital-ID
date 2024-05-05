2da2f47eb3f794d341676ae26e59965edc7441ca
ShreyasPathe <90187847+ShreyasPathe@users.noreply.github.com>
2024-05-05 23:51:27 +0530

    Update id.py

--- id.py
+++ id.py
@@ -15,7 +15,7 @@ def fetch_student_info(student_id):
         with open('student.csv', 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if int(row['stuid']) == student_id:
+                if int(row['stuid']) == _id:
                     return row
         return None
     except Exception as e:
